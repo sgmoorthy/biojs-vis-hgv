17e77aee5e87989bba85a74949113c8395102a13
Saket Choudhary <saketkc@gmail.com>
2015-10-08 22:30:47 -0700

    Add example

---
+++ snippets/P00533.js
@@ -0,0 +1,6 @@
+var app = require("biojs-vis-hgv");
+var hgvViewer = new app({
+                    accession :  "P00533",//O15234",//P15289",//"P45985",
+                    baseUrl: "../data/",
+                    target: yourDiv
+                    });
